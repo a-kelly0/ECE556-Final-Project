9db3c9a56e544a382235df9b08f70f9de17fc454
Anna P <85371495+a-kelly0@users.noreply.github.com>
2026-02-22 17:59:01 -0500

    got simple cnn training

--- food_cnn.py
+++ food_cnn.py
@@ -15,8 +15,14 @@ import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+#Hyperparameters
+input_size = 128*128
+learning_rate = 0.001
+num_epochs = 10
+batch_size = 32
+
 #load datasets
-ds = load_dataset("Scuccorese/food-ingredients-dataset", split="train[:5000]")
+ds = load_dataset("Scuccorese/food-ingredients-dataset", split="train[:500]")
 ds = ds.train_test_split(test_size=0.2) #split data into training data and validation data
 print(ds["train"][0])
 
@@ -24,6 +30,8 @@ print(ds["train"][0])
 ingredients = sorted(set(ds["train"]["ingredient"]))
 label2id = {name: str(i) for i, name in enumerate(ingredients)}
 id2label = {str(i): name for i, name in enumerate(ingredients)}
+
+num_classes = len(ingredients)
 
 print(id2label["0"])
 
@@ -35,19 +43,34 @@ transform = transforms.Compose([
                          [0.5,0.5,0.5]) #TODO normalize to mean of dataset
 ])
 
-def transform_fn(example):
-    image = example["image"].convert("RGB") #make all images RGB to avoid initial channel mismatches
-    example["image"] = transform(image)
-    return example
+def transform_fn(data):
+    image = data["image"].convert("RGB") #make all images RGB to avoid initial channel mismatches
+    data["image"] = transform(image)
+    data["label"] = int(label2id[data["ingredient"]])
+    return data
 
 ds["train"] = ds["train"].map(transform_fn)
 ds["test"]  = ds["test"].map(transform_fn)
 
+#convert to pytorch tensor
+ds["train"] = ds["train"].with_format("torch", columns=["image", "label"])
+ds["test"] = ds["test"].with_format("torch", columns=["image", "label"])
+
 #create dataloaders
-batch_size = 32
 
 train_loader = DataLoader(dataset=ds["train"], batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(dataset=ds["test"], batch_size=batch_size, shuffle=False)
+
+# #display some random training images
+# def imshow(img):
+#     plt.imshow(np.transpose(img.numpy()), (1, 2, 0))
+#     plt.show()
+
+# dataiter = iter(train_loader)
+# images, labels, = next(dataiter)
+
+# imshow(torchvision.utils.make_grid(images))
+
 
 #Simple cnn
 class SimpleCNN(nn.Module):
@@ -73,5 +96,47 @@ class SimpleCNN(nn.Module):
         x = self.fc_layers(x)
         return x
     
+model = SimpleCNN(num_classes=num_classes)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+#only useful if using gpu, add back support for later
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+#Loop through dataset and update the model's weights
+for epoch in range(num_epochs):
+
+    model.train()
+
+    running_loss = 0.0
+    print(f"Epoch [{epoch + 1}/{num_epochs}]")
+    for batch in train_loader:
+        images = batch["image"]
+        labels = batch["label"]
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+#Find the accuracy of the network
+correct = 0
+total = 0
+
+model.eval()
+
+with torch.no_grad():
+    for data in val_loader:
+        images = data["image"]
+        labels = data["label"]
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the 500 test images: {100 * correct // total} %')
 
 
