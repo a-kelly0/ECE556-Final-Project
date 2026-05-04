375e7727d1c032c947751d0d2a77db27d12172c3
Anna P <85371495+a-kelly0@users.noreply.github.com>
2026-05-04 10:56:44 -0400

    added main inference script

---
+++ hw_scripts/reciPI_main.py
@@ -0,0 +1,85 @@
+import json
+import time
+import argparse
+
+import torch
+from torch import nn
+from PIL import Image
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+from picamera2 import Picamera2, Preview
+
+import infer_camera_pi
+
+def main():
+    # Model Files
+    mod = "produce_net_fullmode.pth"
+    labels = "../JSON/id2label_produce.json"
+    norm = "../JSON_norm_stats_produce_newdata.json"
+    topk = 5
+
+    # Welcome message
+    print("------------------------------------------------------------------------")
+    print("                       Welcome to ReciPI!")
+    print("------------------------------------------------------------------------")
+    print("\n")
+
+    # Take pictures of ingredients with PI Camera
+    images = [] #array to temporarily store pictures
+    response = input("How many ingredients would you like to capture? ")
+    capture_mode = True
+    while(capture_mode):
+        if not(response.isdigit()):
+            print("Please input a valid number")
+        elif(int(response == 0)):
+            print("Must have at least one ingredient")
+        else:
+            images = infer_camera_pi.capture_frame_rgb(quantity=int(response)) #take the requested number of pictures with the pi cam
+            capture_mode = False
+
+    # Load model
+        with open(labels, "r") as f: #load label mapping
+            id2label = json.load(f)
+        
+        mean, std = infer_camera_pi.load_norm_stats(norm) #load normalization stats
+
+    device = torch.device("cpu") #run on rpi3 cpu
+
+    model = torch.load(mod, map_location=device, weights_only=False) #load model
+    model.eval()
+
+    # Prep images for inference
+    transform = transforms.Compose([ #Define preprocessing for image before CNN
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std),
+    ])
+
+    transformed_images = []
+    for img in images:
+        transformed_images.append(transform(img)) #apply image transform
+
+    batch = torch.stack(images) #form input tensor
+
+    # Run inference
+    batch = batch.to(device)
+
+    with torch.no_grad():
+        logits = model(batch)
+        probs = torch.softmax(logits, dim=1)[0]
+
+        
+        topk = min(topk, probs.numel())
+        vals, idxs = torch.topk(probs, k=topk) #Top 5 ingredients
+
+    print("Top predictions:")
+    for v, i in zip(vals.tolist(), idxs.tolist()):
+        #JSON keys may be strings
+        name = id2label.get(str(i), id2label.get(i, f"class_{i}"))
+        print(f"  {name}: {v:.3f}")
+
+
+if __name__ == "__main__":
+    main()
+        
