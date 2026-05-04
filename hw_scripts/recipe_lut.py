e903f614834df76371861e0ed60b8d8d915379a2
a-kelly0 <85371495+a-kelly0@users.noreply.github.com>
2026-05-04 17:57:18 -0400

    added link

--- hw_scripts/recipe_lut.py
+++ hw_scripts/recipe_lut.py
@@ -5,13 +5,14 @@ def get_recipe(ingredients:list):
     Given a list of ingredients, searches the recipe dictionary for the top recipe that uses the available ingredients
     Dictionary of valid recipes should be stored as reduced_recipe_cache.pkl in the current dir
     '''
-    with open("reduced_recipe_cache.pkl", "rb") as f:
+    with open("../data/reduced_recipe_cache.pkl", "rb") as f:
         ds = pickle.load(f)
 
     ingredients = set(ingredients)
 
     winner = None
     best_score = -1
+    link = None
 
     #iterate through recipes, rank by how many of the available ingredients are used
     for row in ds:
@@ -20,8 +21,8 @@ def get_recipe(ingredients:list):
         if score > best_score:
             best_score = score
             winner = row["title"]
-    
-    return winner
+            link = row["link"]    
+    return winner, link
 
 available_ingredients = ['strawberries']
 print(get_recipe(available_ingredients))
